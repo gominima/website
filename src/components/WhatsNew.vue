e2c4f155d918c361a9bdf70ed4e8b68258dd111c
Skyascii <savioxavier112@gmail.com>
2022-01-23 12:23:02 +0400

    chore: update base styles for whatsnew component

--- src/components/WhatsNew.vue
+++ src/components/WhatsNew.vue
@@ -1,19 +1,24 @@
 <script lang="ts">
 import { defineComponent } from "@vue/runtime-core";
 export default defineComponent({
-	name : "WhatsNew",
-	setup(){
-
-	}
-})
+  name: "WhatsNew",
+  setup() {},
+});
 </script>
 
-
 <template>
-<div class="flex justify-center pt-12">
- <div class="flex  bg-gray-700 w-96 pt-2 pb-2 space-x-2 items-center justify-center rounded-full">
-  <div class="bg-hero pl-2 pr-2 text-white rounded-full">New</div>
-   <div class=" text-white rounded-md">Added new route system, check it here ></div>
- </div>
- </div>
+  <div class="flex justify-center pt-12">
+    <div
+      class="flex bg-gray-700 w-96 pt-2 pb-2 space-x-2 items-center justify-center rounded-full"
+    >
+      <div class="bg-hero font-medium pl-2 pr-2 text-white rounded-full">
+        New
+      </div>
+      <div class="text-white rounded-md">
+        Added new route system, check it here
+        <!-- Add font awesom cheveron right icon below -->
+        <i class="fas fa-chevron-right text-blue-500"></i>
+      </div>
+    </div>
+  </div>
 </template>
