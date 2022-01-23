3079bd963cdc6debb8c0ae529eb7536aadfd5afd
Skyascii <savioxavier112@gmail.com>
2022-01-23 12:15:25 +0400

    chore: update base styles for navbar component

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,43 +1,60 @@
 <script lang="ts">
-import { defineComponent} from "@vue/runtime-core";
-import {Ref, ref} from "vue"
+import { defineComponent } from "@vue/runtime-core";
+import { Ref, ref } from "vue";
 export default defineComponent({
- name: "Navbar",
- setup(){
-	const Visible:Ref<boolean> = ref<boolean>(false)
-	const Toggle = () => {
-		Visible.value = !Visible.value
-	}
-	window.addEventListener("resize", ()=> {
-		if(window.innerWidth > 768) {
-			Visible.value = false
-		}
-	})
-	return {
-	      Visible: Visible,
-	      Toggle: Toggle
-	}
- }
-})
+  name: "Navbar",
+  setup() {
+    const Visible: Ref<boolean> = ref<boolean>(false);
+    const Toggle = () => {
+      Visible.value = !Visible.value;
+    };
+    window.addEventListener("resize", () => {
+      if (window.innerWidth > 768) {
+        Visible.value = false;
+      }
+    });
+    return {
+      Visible: Visible,
+      Toggle: Toggle,
+    };
+  },
+});
 </script>
 
 <template>
-<div>
- <div class="flex justify-between p-6 bg-mainbg"> 
-	 <div class="Hero text-3xl font-bold text-white">Minima</div>
-	 <ul  class="hidden md:flex flex-row  space-x-10">
-		 <li class="links text-2xl font-semibold text-gray-200">Documentation</li>
-		 <li class="links text-2xl font-semibold text-gray-200">Guide</li>
-		 <button class="bg-hero pt-1 pl-3 pr-3 pb-1 text-white font-semibold text-xl text-center rounded-full">Get started</button>
-	 </ul>
-	 <div class="button flex md:hidden text-3xl">
-           <i @click="Toggle" class="fas fa-ellipsis-v rotate-90 text-white"></i>
-	 </div>
- </div>
-        <ul v-if="Visible"  class="pl-6 space-y-4 justify-start w-32 text-left">
-		 <li class="links text-1xl font-bold text-gray-200">Documentation</li>
-		 <li class="links text-1xl font-bold text-gray-200">Guide</li>
-		 <button class="bg-hero pt-1 pl-2 pr-2 pb-1 text-white text-md text-center rounded-full">Get started</button>
-	 </ul>
-</div>
+  <header class="p-4">
+    <div class="container flex justify-between h-16 mx-auto">
+      <div class="flex items-center">
+        <a href="#" class="text-blue-600 hover:text-blue-700">
+          <span class="text-2xl font-bold tracking-tight">Minima</span>
+        </a>
+      </div>
+      <div class="items-center flex-shrink-0 hidden lg:flex">
+        <button class="self-center px-8 py-3 rounded text-white">
+          Documentation
+        </button>
+        <button
+          class="self-center px-8 py-3 font-semibold rounded bg-blue-600 hover:bg-blue-700 text-white"
+        >
+          Sign up
+        </button>
+      </div>
+      <button class="p-4 lg:hidden">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          class="w-6 h-6 text-blue-100"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M4 6h16M4 12h16M4 18h16"
+          ></path>
+        </svg>
+      </button>
+    </div>
+  </header>
 </template>
