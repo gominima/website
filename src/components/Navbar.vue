b1b55818897d3c4b05ada7452f6dd90cfcd60541
Skyascii <savioxavier112@gmail.com>
2022-01-27 12:17:13 +0400

    chore(navbar): make navbar component responsive

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -29,23 +29,87 @@ export default defineComponent({
           <span class="text-2xl font-bold tracking-tight">Minima</span>
         </a>
       </div>
+
       <div class="items-center flex-shrink-0 hidden lg:flex">
-        <button
+        <a
+          href="#"
           class="self-center py-3 rounded text-white hover:text-gray-200 hover:underline"
         >
           Documentation
-        </button>
-        <button
+        </a>
+        <a
+          href="#"
           class="self-center px-8 py-3 rounded text-white hover:text-gray-200 hover:underline"
         >
           Guide
-        </button>
-        <button
+        </a>
+        <a
+          href="#"
           class="self-center px-8 py-3 font-semibold rounded bg-blue-600 hover:bg-blue-700 text-white"
         >
           Get started
-        </button>
+        </a>
       </div>
+
+      <!-- Mobile menu -->
+      <!-- FIX: Toggle element on mobile -->
+
+      <div
+        class="absolute z-10 top-0 inset-x-0 p-2 transition transform origin-top-right sm:visible md:hidden lg:hidden"
+      >
+        <div
+          class="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden"
+        >
+          <div class="px-5 pt-4 flex items-center justify-between">
+            <div class="-mr-2">
+              <button
+                type="button"
+                class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+              >
+                <span class="sr-only">Close main menu</span>
+                <svg
+                  class="h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
+          <div class="px-2 pt-2 pb-3 space-y-1">
+            <a
+              href="#"
+              class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+              >Documentation</a
+            >
+
+            <a
+              href="#"
+              class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+              >Guide</a
+            >
+          </div>
+          <a
+            href="#"
+            class="block w-full px-5 py-3 text-center font-medium text-blue-600 bg-gray-50 hover:bg-gray-100"
+          >
+            Get started
+          </a>
+        </div>
+      </div>
+
+      <!-- Close Menu button on mobile -->
+      <!-- FIX: Toggle element on mobile -->
+
       <button class="p-4 lg:hidden">
         <svg
           xmlns="http://www.w3.org/2000/svg"
