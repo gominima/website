92276c936d51f5002a48221db63bf5f93c1b55a8
Skyascii <savioxavier112@gmail.com>
2022-01-28 13:03:34 +0400

    chore: update navbar colors

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -54,17 +54,17 @@ export default defineComponent({
       <!-- Mobile menu -->
       <!-- FIX: Toggle element on mobile -->
 
-      <div
-        class="absolute z-10 top-0 inset-x-0 p-2 transition transform origin-top-right sm:visible md:hidden lg:hidden"
+      <!-- <div
+        class="absolute z-10 top-0 inset-x-0 p-2 transition transform origin-top-right sm:visible lg:hidden"
       >
         <div
-          class="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden"
+          class="rounded-lg shadow-md bg-[#0c1f56] ring-1 ring-black ring-opacity-5 overflow-hidden"
         >
           <div class="px-5 pt-4 flex items-center justify-between">
             <div class="-mr-2">
               <button
                 type="button"
-                class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+                class="bg-[#143797] rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
               >
                 <span class="sr-only">Close main menu</span>
                 <svg
@@ -88,27 +88,26 @@ export default defineComponent({
           <div class="px-2 pt-2 pb-3 space-y-1">
             <a
               href="#"
-              class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+              class="block px-3 py-2 rounded-md text-base font-medium text-white hover:text-gray-100 hover:bg-[#0f276c]"
               >Documentation</a
             >
 
             <a
               href="#"
-              class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+              class="block px-3 py-2 rounded-md text-base font-medium text-white hover:text-gray-100 hover:bg-[#0f276c]"
               >Guide</a
             >
           </div>
           <a
             href="#"
-            class="block w-full px-5 py-3 text-center font-medium text-blue-600 bg-gray-50 hover:bg-gray-100"
+            class="block w-full px-5 py-3 text-center font-medium text-blue-400 bg-[#112f82] hover:bg-[#0f276c]"
           >
             Get started
           </a>
         </div>
-      </div>
+      </div> -->
 
       <!-- Close Menu button on mobile -->
-      <!-- FIX: Toggle element on mobile -->
 
       <button class="p-4 lg:hidden">
         <svg
