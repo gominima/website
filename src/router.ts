662c61c2fda4bc911d8902837d87e1951a8deaaf
megatank58 <tanumegatank@gmail.com>
2022-02-13 14:59:40 +0530

    feat: Web HTML5 based router

--- src/router.ts
+++ src/router.ts
@@ -1,11 +1,11 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 import routes from '~pages';
 import nprogress from 'nprogress';
 
 routes.push({ path: '/docs', redirect: '/docs/main' })
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
 });
 
