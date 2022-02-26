2e0f626d26e3abfe3b4f5d6d8bab28be2e21b335
ToTu-Dev <75479355+ToTu-Dev@users.noreply.github.com>
2022-02-26 10:30:09 +0530

    chore: remove whyminima

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <Navbar />
   <router-view />
+  <Footer/>
 </template>
 
 <script lang="ts">
 import Navbar from './components/Navbar.vue';
+import Footer from "./components/Footer.vue"
 export default {
-  components: { Navbar }
+  components: { Navbar, Footer }
 }
 </script>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,15 +2,14 @@
   <section class="min-w-screen min-h-screen">
     <WhatsNew />
     <Hero />
-    <WhyMinima/>
   </section>
 </template>
 
 <script lang="ts">
 import WhatsNew from '../components/WhatsNew.vue';
 import Hero from '../components/Hero.vue';
-import WhyMinima from "../components/WhyMinima.vue"
+
 export default {
-  components: { WhatsNew, Hero, WhyMinima},
+  components: { WhatsNew, Hero},
 };
 </script>
