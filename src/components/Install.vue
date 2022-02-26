6c8ff3eed4e63513f602d6416b02d2d22fd4772f
ToTu-Dev <75479355+ToTu-Dev@users.noreply.github.com>
2022-02-26 23:15:08 +0530

    chore: code cleanup

--- src/components/Install.vue
+++ src/components/Install.vue
@@ -1,5 +1,5 @@
 <template>
- <div class="min-h-screen min-w-full flex flex-col justify-center items-center space-y-6">
+ <div class="min-h-screen min-w-full flex flex-col justify-center items-center space-y-6 pb-16 pt-16">
  <div class="text-5xl md:text-7xl font-mons pt-12">Install Minima</div>
  <div class="w-1/2 md:w-1/2 font-semibold md:text-xl text-center">No dependencies, pure net/http with custom handler. Can be used as a net/http wrapper or as an independent framework</div>
   <div class="text-2xl font-bold pt-2">Install minima as go package</div>
@@ -7,9 +7,15 @@
   <pre data-prefix="$"><code>go get github.com/gominima/minima</code></pre>
  </div>
  <div class="text-2xl font-bold pt-2">Create simple minima server</div>
-  <div class="mockup-code bg-code pb-12 w-1/3">
+  <div class="mockup-code bg-code w-1/3">
   <pre ><code v-html="setupRelatedCode"></code></pre>
  </div>
+  <a
+   class="btn btn-primary text-4xl border border-transparent tansition ease-in-out transform hover:-translate-y-1 hover:scale-105 duration-200 normal-case"
+    href="https://guide.gominima.studio"
+   >
+        Install Guide
+      </a>
  </div>
 </template>
 
