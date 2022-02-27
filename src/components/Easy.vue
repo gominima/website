2889a13595d2f157c47c442e1e719a5d2f8f65f2
ToTu-Dev <75479355+ToTu-Dev@users.noreply.github.com>
2022-02-27 11:22:32 +0530

    feat: contributor component

--- src/components/Easy.vue
+++ src/components/Easy.vue
@@ -1,13 +1,13 @@
 <template>
  <div class="bg-neutral min-h-screen min-w-full text-neutral-content  flex justify-center items-center">
-  <div class="flex flex-col justify-center items-center space-y-6 pl-1.5 pr-1.5 pt-16 pb-16">
-  <div class="text-5xl md:text-7xl font-mons">Easy to understand</div>
-  <div class="tet-3xl md:text-4xl font-mons">using minima's utility layer on top of net/http</div>
+  <div class="flex flex-col justify-center items-center space-y-6 pt-16 pb-16">
+  <div class="text-4xl md:text-7xl font-mons text-primary">Easy to understand</div>
+  <div class="tet-3xl md:text-4xl font-mons text-secondary">using minima's utility layer on top of net/http</div>
   <div class="w-2/3 md:w-1/2 font-semibold md:text-lg text-center">Minima is based on hash table which makes it impressivly fast and the framework itself is fully based on net/http with 0 external dependencies.</div>
-  <div class="mockup-code bg-code w-2/3  text-sm ml-4">
+  <div class="mockup-code bg-code w-2/3  text-sm ">
   <pre ><code v-html="setupRelatedCode"></code></pre>
  </div> 
- <div class="divider text-neutral-content text-opacity-30">↓</div>
+  <div class="divider">↓</div>
    <div class="border mockup-window border-base-900 w-2/3">
   <div class="flex justify-center px-4 py-16 border-t border-base-300 text-5xl md:text-7xl">Hello World!</div>
   </div>  
