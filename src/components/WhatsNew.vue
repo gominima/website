2743da6ff0c02fbb687909f6289113bf64082397
ToTu-Dev <75479355+ToTu-Dev@users.noreply.github.com>
2022-03-01 22:44:25 +0530

    chore: update whats new message

--- src/components/WhatsNew.vue
+++ src/components/WhatsNew.vue
@@ -1,16 +1,19 @@
 <script lang="ts">
 import { defineComponent } from 'vue-demi';
 export default defineComponent({
-  name: 'WhatsNew'
+  name: 'WhatsNew',
+
 });
 </script>
 
 <template>
   <div class="flex justify-center pt-12">
     <div class="flex justify-center items-center w-64 md:w-96 lg:w-96 bg-base-300 py-2 px-2 rounded-full">
-      <div class="pt-1 pb-1 p-4 rounded-full btn-primary">New</div>
+      <div class="pt-1 pb-1 p-4 rounded-full btn-secondary">New</div>
       <span class="truncate text-base-content ml-2">
-        Added new route system, check it here
+        <a href="https://github.com/gominima/minima">
+        Version 1 is ready to be launched 🚀
+        </a>
       </span>
       <i
         class="fas fa-chevron-right flex justify-center items-center ml-2"
@@ -18,3 +21,4 @@ export default defineComponent({
     </div>
   </div>
 </template>
+
